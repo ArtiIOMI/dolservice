e459475a95b972284ecefd9842f38dce66d28586
Artur Lorkowski <arti2895@gmail.com>
2024-08-08 15:16:23 +0200

    Usuwam niepotrzebne pliki

--- dolservice/dolservice/doctype/tabela_zgloszenia_rma/tabela_zgloszenia_rma.py
+++
@@ -1,9 +0,0 @@
-# Copyright (c) 2024, Artiom and contributors
-# For license information, please see license.txt
-
-# import frappe
-from frappe.model.document import Document
-
-
-class TabelaZgloszeniaRMA(Document):
-	pass

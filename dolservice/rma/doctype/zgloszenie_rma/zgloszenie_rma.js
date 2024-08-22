f4032e93a94b01dde9de7edf4bfae9bd9056fe8b
Artur Lorkowski <arti2895@gmail.com>
2024-08-22 20:27:50 +0200

    Poprawki dla obowiązkowych informacji w formularzu i dokumencie, które się nie pokrywają

--- dolservice/rma/doctype/zgloszenie_rma/zgloszenie_rma.js
+++ dolservice/rma/doctype/zgloszenie_rma/zgloszenie_rma.js
@@ -6,6 +6,12 @@ frappe.ui.form.on("Zgloszenie RMA", {
         
 	},
     onload: function(frm) {
+        frm.set_df_property('otrzymano_na_magazynie', 'reqd', 'true');
+        frm.set_df_property('podmiot', 'reqd', 'true');
+        frm.set_df_property('nazwa_firmy', 'reqd', 'true');
+        frm.set_df_property('email', 'reqd', 'true');
+        frm.set_df_property('serwisowane_urządzenia', 'reqd', 'true');
+
         //const tour_name = 'Poradnik Zgłoszenia RMA';
         //frm.tour.init({ tour_name }).then(() => frm.tour.start());
     },

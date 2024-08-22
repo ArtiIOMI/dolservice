e815e999a6a637a7501e8d4a4e3d398122f5bf47
Artur Lorkowski <arti2895@gmail.com>
2024-08-22 21:25:48 +0200

    Obowiązkowe pola w formularzu oraz weryfikacja danych.

--- dolservice/rma/web_form/zgłoszenie_rma/zgłoszenie_rma.js
+++ dolservice/rma/web_form/zgłoszenie_rma/zgłoszenie_rma.js
@@ -1,3 +1,25 @@
 frappe.ready(function() {
-	// bind events here
+	frappe.web_form.validate = () => {
+		let error = false;
+		if(!frappe.web_form.get_value('regulamin')){
+			frappe.msgprint('Aby zgłosić RMA, musisz zatwierdzić Regulamin!');
+			error = true;
+		}
+		if(!frappe.web_form.get_value('dane_osobowe')){
+			frappe.msgprint('Aby zgłosić RMA, musisz wyrazić zgodę Danych Osobowych!');
+			error = true;
+		}
+		if(!frappe.web_form.get_value('email').includes('@')){
+			frappe.msgprint('Adres E-Mail jest nie prawdziwy!');
+			error = true;
+		}
+		if(frappe.web_form.get_value('telefon_kontaktowy').length < 9){
+			frappe.msgprint('Numer telefonu jest za krótki!');
+			error = true;
+		}
+
+		if(error){
+			return false;
+		}
+	}
 })

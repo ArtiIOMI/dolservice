236c56bd167101fcb60b2f159ffdcf81233e8802
Artur Lorkowski <arti2895@gmail.com>
2024-08-22 19:20:55 +0200

    Ustalanie szerokości kolumn w tabeli RMA. Przygotowanie dla Poradnika

--- dolservice/rma/doctype/zgloszenie_rma/zgloszenie_rma.js
+++ dolservice/rma/doctype/zgloszenie_rma/zgloszenie_rma.js
@@ -1,8 +1,12 @@
 // Copyright (c) 2024, Artiom and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("Zgloszenie RMA", {
-// 	refresh(frm) {
-
-// 	},
-// });
+frappe.ui.form.on("Zgloszenie RMA", {
+	refresh(frm) {
+        
+	},
+    onload: function(frm) {
+        //const tour_name = 'Poradnik Zgłoszenia RMA';
+        //frm.tour.init({ tour_name }).then(() => frm.tour.start());
+    },
+});

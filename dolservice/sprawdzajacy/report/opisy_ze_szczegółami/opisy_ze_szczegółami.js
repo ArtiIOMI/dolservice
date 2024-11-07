4e5871ca9aa048b8140e1b539d84ec4937c3e157
ArtiIOMI <arti2895@gmail.com>
2024-11-07 16:19:33 +0100

    Wstępna wersja listy opisanych laptopów dla handlowców

---
+++ dolservice/sprawdzajacy/report/opisy_ze_szczegółami/opisy_ze_szczegółami.js
@@ -0,0 +1,49 @@
+// Copyright (c) 2024, Artiom and contributors
+// For license information, please see license.txt
+
+frappe.query_reports["Opisy ze szczegółami"] = {
+	"filters": [
+		{
+			"fieldname": "grade_matrycy",
+			"label": __("Matryca"),
+			"fieldtype": "Select",
+			"width": 50,
+			"reqd": 0,
+			"default": "",
+			"options": [
+				'',
+				'A',
+				'A-',
+				'B',
+				'C'
+				],
+		},
+		{
+			"fieldname": "grade_obudowy",
+			"label": __("Obudowa"),
+			"fieldtype": "Select",
+			"width": 50,
+			"reqd": 0,
+			"default": "",
+			"options": [
+				'',
+				'A',
+				'A-',
+				'B',
+				'C'
+				],
+		},
+		{
+			"fieldname": "uklad_klawiatury",
+			"label": __("Układ Klaw."),
+			"fieldtype": "Select",
+			"width": 50,
+			"reqd": 0,
+			"default": "",
+			"options": [
+				'Inne',
+				'PL'
+				],
+		}
+	]
+};
